4e0f3bd685ee4b7e1f06738adb00ea8fafe829ca
VVP-code <vovochka.ackord@gmail.com>
2025-08-22 20:24:15 +0300

    Delete newvocal directory

--- newvocal/vocal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path, register_converter
-
-from . import converters
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('about/', )
-]
--- newvocal/vocal/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-
-class HomeView(ListView):
-    template_name = "recipe/home.html"
-
-
-# Create your views here.
